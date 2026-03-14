b916813edfd68d3ca6f2f8904ecf01c284a0f2f6
Akash A Pandya <91130000+akashpandya@users.noreply.github.com>
2026-03-13 21:32:45 -0400

    Delete Bellabeat-Fitness-Analysis directory

--- Bellabeat-Fitness-Analysis/user_behavior_analysis.sql
+++
@@ -1,9 +0,0 @@
--- Analyze user activity behavior: Daily step count & active minutes
-SELECT 
-    user_id,
-    ROUND(AVG(steps), 0) AS avg_daily_steps,
-    ROUND(AVG(active_minutes), 1) AS avg_active_minutes_per_day,
-    ROUND(AVG(calories), 1) AS avg_daily_calories
-FROM bellabeat_data
-GROUP BY user_id
-ORDER BY avg_daily_steps DESC;
